9f8877a97cc99b049ceeff9c7f2d830677b1be84
John Mwangi <mail.mwangi@gmail.com>
2025-04-19 08:26:59 +0300

    Implement browsing task

--- llm_browser/main.py
+++ llm_browser/main.py
@@ -1,6 +1,7 @@
 """Uses an LLM model to autonomously browse the Internet"""
 
 import asyncio
+import json
 import logging
 import os
 from datetime import datetime
@@ -8,12 +9,8 @@ from zoneinfo import ZoneInfo
 
 from browser_use import Browser, BrowserConfig
 from dotenv import load_dotenv
-from langchain_anthropic import ChatAnthropic
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_ollama import ChatOllama
-from langchain_openai import ChatOpenAI
 from src import utils
-from src.utils import TaskType, set_logging
+from src.utils import TaskType, models, set_logging
 
 load_dotenv(override=True)
 
@@ -21,20 +18,13 @@ set_logging()
 logger = logging.getLogger(__name__)
 
 tz = os.environ.get("TZ")
-model = os.environ.get("MODEL")
+text_model = os.environ.get("TEXT_MODEL")
+vision_model = os.environ.get("VISION_MODEL")
 max_input_tokens = int(os.environ.get("MAX_INPUT_TOKENS", 120000))
 headless = bool(int(os.environ.get("HEADLESS"), 0))
 browser = Browser(config=BrowserConfig(headless=headless))
 ts = datetime.now(tz=ZoneInfo(tz)).strftime("%Y-%m-%d_%H%M%S")
-discord_webhook = os.environ.get("DISCORD_WEBHOOK")
-
-models = {
-    "openai": ChatOpenAI(model="gpt-4o-mini"),
-    "anthropic": ChatAnthropic(model_name="claude-3-5-sonnet-20241022"),
-    "ollama": ChatOllama(model="llama3.2:latest"),
-    "gemini-vision": ChatGoogleGenerativeAI(model="gemini-2.0-pro-exp-02-05"),
-    "gemini-text": ChatGoogleGenerativeAI(model="gemini-1.5-flash"),
-}
+webhook = os.environ.get("DISCORD_WEBHOOK")
 
 
 def main():
@@ -57,38 +47,49 @@ def main():
                 raise ValueError(f"unknown task: {task}")
 
             title = doc["title"]
-
-            # prompt = prompts.find_one({"type": "google"}, collation={"strength": 2, "locale": "en"})
-            # prompt = prompts.find_one(
-            #     {"type": {"$regex": "^google$", "$options": "i"}}
-            # )["prompt"]
-            prompt = prompts.find_one({"type": "google_augmented"})["prompt"]
-            resume = resumes.find_one({"type": "data engineer"})["resume"]
+            resume_content = resumes.find_one({"type": "data engineer"})["resume"]
+            resume = {"resume": resume_content}
+            resume_prompt = prompts.find_one({"type": "google_augmented"})["prompt"]
 
             if task == TaskType.BROWSE:
-                asyncio.run(
+                main_prompt = prompts.find_one({"type": "browse"})["prompt"]
+                url = doc["url"]
+                browsing_prompt = main_prompt + "\n\nURL to navigate: " + url
+
+                result = asyncio.run(
                     utils.browse_content(
-                        prompt=prompt,
-                        path=None,
-                        model=models.get(model),
+                        prompt=browsing_prompt,
+                        model=models.get(vision_model),
                         browser=browser,
                         max_input_tokens=max_input_tokens,
-                        ts=ts,
                     )
                 )
 
+                augmented_data = {**result, **resume}
+
+                response = utils.query_llm(
+                    data=augmented_data,
+                    prompt=resume_prompt,
+                    model=models.get(text_model),
+                )
+
+                utils.post_response(response=response, webhook=webhook, title=title)
+
             if task == TaskType.SCRAPE:
+                # prompt = prompts.find_one({"type": "google"}, collation={"strength": 2, "locale": "en"})
+                # prompt = prompts.find_one(
+                #     {"type": {"$regex": "^google$", "$options": "i"}}
+                # )["prompt"]
+
                 data = utils.download_content_google(
                     prompt_context=dict(doc), headless=headless
                 )
                 response = utils.query_llm(
-                    data={**data, **{"resume": resume}},
-                    prompt=prompt,
-                    model=models.get("gemini-text"),
+                    data={**data, **resume},
+                    prompt=resume_prompt,
+                    model=models.get(text_model),
                 )
-                utils.post_response(
-                    response=response, webhook=discord_webhook, title=title
-                )
+                utils.post_response(response=response, webhook=webhook, title=title)
 
 
 if "__name__" == "__name__":
