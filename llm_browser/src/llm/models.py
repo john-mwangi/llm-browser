5704b429a428621f86bd851663c2b81a66562758
John M. <mail.mwangi@gmail.com>
2025-06-08 22:06:26 +0300

    Test Ollama model (#35)
    * Update models.py
    
    * Filter roles by rating
    
    Closes #14
    
    * Add helper function to uploade files or multiline strings
    
    * Save contents to db
    
    * Update database.py
    
    * Refactor the notificaion decorator
    
    * Update to handle revised prompt
    
    * Update utils.py
    
    * Update utils.py
    
    * Save model names to db
    
    This can help with model evaluation later on
    
    * Specify context
    
    Closes #15
    
    * Update type hints
    
    * Add prompt template
    
    * Add CLI to extract transcript
    
    * Post each item as a separate notification
    
    Closes #26
    
    * Scrape LI while logged in
    
    * Open new tab instead of new browser instance
    
    * Add LI tests
    
    * Rename test module
    
    * Update tests
    
    * Rename test
    
    * Update Google tests
    
    * Refactor Google fn to pass new tests
    
    * Add browse_content test
    
    * Update test documentation
    
    * Fixes (#30)
    
    * Handle empty query (no links)
    
    * Update logs
    
    * Update docs
    
    * Fix error
    
    * Update tests
    
    * Deprecate the docker file
    
    Requires a screen - can be deployed on vm w/o a display
    
    * Update readme
    
    * Rename tests
    
    * Use Ollama in remote server
    
    Closes #11
    
    * Test Ollama model

--- llm_browser/src/llm/models.py
+++ llm_browser/src/llm/models.py
@@ -1,14 +1,25 @@
 """Definition and initialization of LangChain models (ChatOpenAI, ChatGoogleGenerativeAI, etc.)"""
 
+import os
+
+from dotenv import load_dotenv
 from langchain_anthropic import ChatAnthropic
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_ollama import ChatOllama
 from langchain_openai import ChatOpenAI
 
+load_dotenv()
+
+host = os.environ.get("_MONGO_HOST")
+port = os.environ.get("_OLLAMA_PORT")
+base_url = f"http://{host}:{port}"
+
 models = {
     "openai": ChatOpenAI(model="gpt-4o-mini"),
     "anthropic": ChatAnthropic(model_name="claude-3-5-sonnet-20241022"),
-    "ollama": ChatOllama(model="llama3.2:latest"),
+    "ollama": ChatOllama(
+        model="llama3.2:3b", base_url=base_url, disable_streaming=True
+    ),
     "gemini-vision": ChatGoogleGenerativeAI(model="gemini-2.0-flash"),
     "gemini-text": ChatGoogleGenerativeAI(model="gemini-1.5-flash"),
 }
